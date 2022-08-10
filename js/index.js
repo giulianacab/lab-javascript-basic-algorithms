28620d5389f11b83931482ee6a8e5c109026666a
Giuliana Cabral <cabral.giu@hotmail.com>
2022-08-10 11:10:41 -0300

    LAB JavaScript Basic Algorithms

--- js/index.js
+++ js/index.js
@@ -1,6 +1,55 @@
 // Iteration 1: Names and Input
+// 1.1 Create a variable hacker1 with the driver's name.
+// 1.2 Print "The driver's name is XXXX".
+// 1.3 Create a variable hacker2 with the navigator's name.
+// 1.4 Print "The navigator's name is YYYY".
+
+const hacker1 = "Giuliana"
+console.log("The driver's name is " + hacker1)
+
+const hacker2 = "Beatriz"
+console.log("The navigator's name is " + hacker2)
+
+
 //
-// Iteration 2: Conditionals
+// Iteration 2: Conditionals\
+// 2.1. Depending on which name is longer, print:
+// - The driver has the longest name, it has XX characters. or
+// - It seems that the navigator has the longest name, it has XX characters. or
+// - Wow, you both have equally long names, XX characters!.
+
+console.log(hacker1.length)
+console.log(hacker2.length)
+
+if (hacker1.length > hacker2.length) {
+    console.log("The driver has the longest name, it has " + hacker1.length + " characters")
+} else {
+    console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters")
+}
 
 
+
+//
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+console.log(hacker1.toUpperCase().split("").join(" "));
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+console.log(hacker2.split('').reverse().join(''));
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+// - The driver's name goes first.
+// - Yo, the navigator goes first definitely.
+// - What?! You both have the same name?
+
+if (hacker1 > hacker2) {
+    console.log("The driver's name goes first.")
+  }
+  else if (hacker1 < hacker2) {
+    console.log("Yo, the navigator goes first definitely.")
+  }
+  else {
+    console.log("What?! You both have the same name?")
+  } 
